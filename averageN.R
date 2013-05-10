e075f0aee60eb25ff7e4b49b267e58eefb2a745b
Martin Ku <martin79831@gmail.com>
2013-05-10 23:55:36 +0800

    Fixed #4. Used function.

--- averageN.R
+++ averageN.R
@@ -1,33 +1,34 @@
-# Parameters (Set up by users)
-n = 5
-
-# Read input data
-file = "~/Projects//BitBucket/SET//Chinese_Weekday_Drama.csv"
-chinese_drama_ratings = read.csv(file, fileEncoding="utf-8")
-cdr = chinese_drama_ratings
-
-# Calculate the results
-nrows = nrow(cdr)
-ncols = ncol(cdr)
-prediction = cdr
-# We cannot predict these entries
-prediction[1:n,] = NA
-for(r in (n+1):nrows)
-{
-  for(c in 1:ncols)
+avgN = function(n=5,...){
+  # n: can be 1,2,3,... (note that 5 is the best for cdr)
+  
+  # Read input data
+  file = "~/Projects//BitBucket/SET//Chinese_Weekday_Drama.csv"
+  chinese_drama_ratings = read.csv(file, fileEncoding="utf-8")
+  cdr = chinese_drama_ratings
+  
+  # Calculate the results
+  nrows = nrow(cdr)
+  ncols = ncol(cdr)
+  prediction = cdr
+  # We cannot predict these entries
+  prediction[1:n,] = NA
+  for(r in (n+1):nrows)
   {
-    prediction[r,c] = mean(cdr[(r-n):(r-1),c])
+    for(c in 1:ncols)
+    {
+      prediction[r,c] = mean(cdr[(r-n):(r-1),c])
+    }
   }
+  
+  # Compute MAPE
+  diff = cdr - prediction
+  absdiff = abs(diff)
+  absp = absdiff/cdr
+  mapes = colMeans(absp,na.rm=TRUE)
+  mape = mean(mapes)
+  
+  # Print MAPE
+  print(mapes)
+  cat("\nAvgN: ",n)
+  cat("\nMAPE:", mape)
 }
-
-# Compute MAPE
-diff = cdr - prediction
-absdiff = abs(diff)
-absp = absdiff/cdr
-mapes = colMeans(absp,na.rm=TRUE)
-mape = mean(mapes)
-
-# Print MAPE
-print(mapes)
-cat("\nAvgN: ",n)
-cat("\nMAPE:", mape)
