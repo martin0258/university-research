543aa81358b12f85b18ff13284a5362ccd854336
martin.ku <martin79831@gmail.com>
2014-01-21 09:08:29 +0800

    Encapsulate model initializations into methods.

--- src/dynamicAdaboostR2.py
+++ src/dynamicAdaboostR2.py
@@ -17,6 +17,16 @@ from lib.windowing import windowing
 from lib.mape import mape
 
 
+def decision_tree():
+    return DecisionTreeRegressor(max_depth=4)
+
+
+def adaboost_r2():
+    rng = np.random.RandomState(1)
+    return AdaBoostRegressor(DecisionTreeRegressor(max_depth=4),
+                             n_estimators=300, random_state=rng)
+
+
 if __name__ == '__main__':
     # TODO: remove hard code
     home = expanduser("~")
@@ -28,9 +38,7 @@ if __name__ == '__main__':
     # Read the dataset
     data = pd.read_csv(rating_filepath)
 
-    rng = np.random.RandomState(1)
-
-    # loop through each drama
+    # Loop through each drama
     for col in data.columns:
         ratings = data[col].values
         w_ratings = windowing(ratings, window_len)
@@ -38,10 +46,12 @@ if __name__ == '__main__':
         # FIXME: the actual num of episodes should come from drama definition
         num_episodes = np.max(np.where(np.isnan(ratings) == False))
         num_cases = num_episodes - window_len + 1
+        # Loop through each episode to be predicted
         for test_index in range(1, num_cases):
             test_episode = test_index + window_len
 
             # Deal with missing values
+            # TODO: Survey specific approach for time series data
             imp = Imputer(missing_values='NaN', strategy='mean', axis=0)
             new_w_ratings = imp.fit_transform(w_ratings[:test_index + 1])
 
@@ -53,16 +63,16 @@ if __name__ == '__main__':
             drama_name = col.decode("utf-8")
             print drama_name, ':',
 
-            clf_1 = DecisionTreeRegressor(max_depth=4)
-            clf_2 = AdaBoostRegressor(DecisionTreeRegressor(max_depth=4),
-                                      n_estimators=300, random_state=rng)
+            model_1 = decision_tree()
+            model_2 = adaboost_r2()
+
             print "training...",
-            clf_1.fit(train_x, train_y)
-            clf_2.fit(train_x, train_y)
+            model_1.fit(train_x, train_y)
+            model_2.fit(train_x, train_y)
 
             print "testing episode %d..." % test_episode,
-            y_1 = clf_1.predict(test_x)
-            y_2 = clf_2.predict(test_x)
+            y_1 = model_1.predict(test_x)
+            y_2 = model_2.predict(test_x)
 
             print "done!\n",
         # End of for
