7e02fd078e91370c70ef5c3c3fd7966f73398c98
martin.ku <martin79831@gmail.com>
2014-04-01 20:42:17 +0800

    Update feature experiemnts

--- src/featureExperiment.R
+++ src/featureExperiment.R
@@ -1,35 +1,59 @@
-featureExperiment = function(ratingFile, featureFiles, settingFile) {
-  # Return a data frame containing all features joined by column Drama & Episode.
+featureExperiment = function (ratingFile, featureFiles, featureSettingFile,
+                              featureSuffixes=c('0','1','2','3')) {
+  # Perform feature selection experiments.
   #
   # Args:
   #   ratingFile: a rating file
   #   featureFiles: a list of feature files
-  #   settingFile: a feature setting file
+  #   featureSettingFile: a feature setting file
+  #   featureSuffixes: column names with one of the suffix will be included
   #
   # Returns:
   #   No return. It will write experiment results into various files.
   
-  allFeatures <- getFeatures(featureFiles)
-  settings <- read.csv(settingFile, fileEncoding="utf-8")
-  settingFeatures <- names(settings)
-  # TODO: find common columns dynamically
-  commonColumns <- c("Drama", "Episode")
-  commonFilter <- rep(TRUE, length(commonColumns))
-  selectedColumns <- c(commonColumns, settingFeatures)
-  features <- subset(allFeatures, select = selectedColumns)
-  
-  # Loop through all settings
-  for(i in 1:nrow(settings))
-  {
-    setting <- c(commonFilter, as.logical(settings[i,]))
-    subFeatures <- features[, setting]
-    if(ncol(subFeatures)==length(commonColumns))
-    {
-      subFeatures <- NULL
+  # Get a list of data frames from feature files (one each)
+  featureList <- getFeatures(featureFiles, featureSuffixes)
+  features <- featureList$data
+  numCases <- nrow(features[[1]])
+
+  # Read settings. Each row is an experiment.
+  featureSettings <- read.csv(featureSettingFile, fileEncoding="utf-8")
+  for (i in 2:nrow(featureSettings)) {
+    # Compose the features used at this experiment
+    setting <- featureSettings[i, ]
+    if (length(setting) != length(features)) {
+      stop("Number of column mismatched between feature and its setting.")
     }
-    result <- dynamicARIMA(ratingFile, subFeatures)
-    fileName <- sprintf("%s.csv", paste(settings[i,], collapse=''))
+    featureUsed <- data.frame(row.names=1:numCases)
+
+    for (j in 1:length(setting)) {
+      if (setting[j]=='1') {
+        featureUsed <- cbind(featureUsed, features[[j]])
+      }
+    }
+    if (ncol(featureUsed) == 0) {
+      featureUsed <- NULL
+    } else {
+      featureUsed <- cbind(featureList$selector, featureUsed)
+    }
+
+    # Run experiment
+    result <- dynamicARIMA(ratingFile, featureUsed)
+
+    # Write result into files
+    fileName <- sprintf("%s.csv", paste(setting, collapse=''))
     write.table(t(result$mape), fileName, row.names = FALSE)
     write.table(result$forecast, fileName, append = TRUE, row.names = FALSE)
   }
 }
+
+# Example:
+# Assume the working directory is "data"
+ratingFile <- "Chinese_Drama_Ratings.csv"
+featureFiles <- c("Chinese_Drama_Opinion.csv",
+                  "Chinese_Drama_GoogleTrend.csv",
+                  "Chinese_Drama_FB.csv")
+featureSettingFile <- "../featureSetting.csv"
+featureSuffixes <- c('1', '2', '3')
+featureExperiment(ratingFile, featureFiles,
+                  featureSettingFile, featureSuffixes)
