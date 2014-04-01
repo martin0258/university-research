5ea033018e5303a1bba6c5008d004cdc70c3943c
martin.ku <martin79831@gmail.com>
2014-04-01 20:44:09 +0800

    Update function to get features

--- src/getFeature.R
+++ src/getFeature.R
@@ -1,24 +1,45 @@
-getFeatures = function(featureFiles) {
-  # Return a data frame containing all features joined by column Drama & Episode.
+getFeatures <- function (featureFiles, featureSuffixes,
+                        suffixSeparator='_') {
+  # Return a data frame containing all features joined by common variables.
   #
   # Args:
   #   featureFiles: a list of feature files
+  #   featureSuffixes: column names with one of the suffix will be included
+  #   suffixSeparator: a character appears before the feature suffixes
   #
   # Returns:
-  #   A data frame of column Drama, Episode, and feature columns.
-  
-  # We use join_all to merge different feature files
-  library(plyr)
+  #   A data frame joined by common variables and filtered by suffixes
   
   dataFrames <- list()
   for(featureFile in featureFiles)
   {
     # Read input data
-    dataFrame <- read.csv(featureFile, fileEncoding="utf-8")
-    dataFrames[[length(dataFrames)+1]] <- dataFrame
+    features <- read.csv(featureFile, fileEncoding="utf-8")
+
+    # Assumption: we assume same row in different files represent same case
+
+    # Filter column by suffixes
+    columnsSelected <- vector()
+    columnNames <- names(features)
+    for (i in 1:length(columnNames)) {
+      # Add into columns selected if it matches one of suffixes
+      nameSplits <- unlist(strsplit(columnNames[i], suffixSeparator))
+      splitLen <- length(nameSplits)
+      if (splitLen > 1) {
+        # Length is greater than 1 means it has suffix
+        suffix <- nameSplits[splitLen
+                             ]
+        if (suffix %in% featureSuffixes) {
+          columnsSelected <- c(columnsSelected, columnNames[i])
+        }
+      }
+    }
+
+    dataFrames[[length(dataFrames)+1]] <- subset(features,
+                                                 select=columnsSelected)
   }
 
-  # Join by common variables (expectation: Drama, Episode)
-  features <- join_all(dataFrames)
-  return(features)  
+  # FIXME: remove hard-coded selector
+  features <- read.csv(featureFiles[1], fileEncoding="utf-8")
+  return (list(selector=features[, c(1, 2)], data=dataFrames))
 }
