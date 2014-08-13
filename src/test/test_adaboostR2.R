092f5d47605005065bfc2c71ca9de4f0f1596ec5
Martin Ku <martin.ku@skysource.com.tw>
2014-08-13 08:33:25 +0800

    Test TrAdaBoost.R2 with UCI data

--- src/test/test_adaboostR2.R
+++ src/test/test_adaboostR2.R
@@ -1,6 +1,7 @@
 library(testthat)
-context("Test AdaBoost.R2")
+context("Test transfer learning algorithms with UCI data")
 source("../adaboostR2.R")
+source("../trAdaboostR2.R")
 library(pracma)
 
 # Precondition: 
@@ -39,19 +40,37 @@ uci_test <- function(file_prefix, predictor, ...) {
       uci_train_data <- rbind(uci_train_data, uci_data[[j]])
     }
 
+    # prepare source data (for transfer learning algorithm)
+    uci_train_src_data <- uci_train_data
+    
     # add training instances from target data set
     num_training_from_target <- 25
-    uci_train_data <- rbind(uci_train_data,
-                            uci_data[[i]][1:num_training_from_target, ])
-
+    train_from_target <- uci_data[[i]][1:num_training_from_target, ]
+    uci_train_data <- rbind(uci_train_data, train_from_target)
+    
+    # prepare target data (for transfer learning algorithm)
+    uci_train_target_data <- train_from_target
+    
     # prepare testing data
     uci_test_data <- uci_data[[i]][-1:-num_training_from_target, ]
     num_test_cases <- nrow(uci_test_data)
     num_features <- ncol(uci_data[[i]]) - 1
     formula <- as.formula(sprintf('%s ~ .', response))
-    fit <- predictor(formula, uci_train_data, ...)
+    
+    # fit with training data
+    predictor_name <- as.character(substitute(predictor))
+    if (predictor_name == "trAdaboostR2") {
+      fit <- predictor(formula,
+                       source_data=uci_train_src_data,
+                       target_data=uci_train_target_data, ...)
+    } else {
+      fit <- predictor(formula, uci_train_data, ...)
+    }
+    
+    # predict on training & testing data
     train_prediction <- predict(fit, uci_train_data)
     test_prediction <- predict(fit, uci_test_data)
+    
     test_errors <- rmserr(test_prediction, uci_test_data[[response]])
     train_errors <- rmserr(train_prediction, uci_train_data[[response]])
     training_rmse <- c(training_rmse, round(train_errors$rmse, 2))
@@ -61,37 +80,52 @@ uci_test <- function(file_prefix, predictor, ...) {
   cat(' Testing  RMSE: ', paste(testing_rmse, collapse=' | '), '\n')
 }
 
+
+# TODO: avoid declaring duplicate nnet parameters in every test
+
 test_that('UCI Conrete Length data', {
   file_prefix <- 'new-concrete'
   set.seed(seed)
   cat(sprintf('-- Data: %s -----------------------', file_prefix), '\n')
+  
   cat(sprintf('-- Model: nnet -----------------------'), '\n')
-  # TODO: avoid declaring duplicate parameters
   uci_test(file_prefix, nnet, size=7, linout=T, trace=F)
+  
   cat(sprintf('-- Model: AdaBoost.R2 with nnet ------'), '\n')
   uci_test(file_prefix, adaboostR2, size=7, linout=T, trace=F)
+  
+  cat(sprintf('-- Model: TrAdaBoost.R2 with nnet ------'), '\n')
+  uci_test(file_prefix, trAdaboostR2, size=7, linout=T, trace=F)
 })
 
 test_that('UCI Housing data', {
   file_prefix <- 'new-housing'
   set.seed(seed)
   cat(sprintf('-- Data: %s -----------------------', file_prefix), '\n')
+  
   cat(sprintf('-- Model: nnet -----------------------'), '\n')
-  # TODO: avoid declaring duplicate parameters
   uci_test(file_prefix, nnet, size=12, linout=T, trace=F)
+  
   cat(sprintf('-- Model: AdaBoost.R2 with nnet ------'), '\n')
   uci_test(file_prefix, adaboostR2, size=12, linout=T, trace=F)
+  
+  cat(sprintf('-- Model: TrAdaBoost.R2 with nnet ------'), '\n')
+  uci_test(file_prefix, trAdaboostR2, size=12, linout=T, trace=F)
 })
 
 test_that('UCI Auto MPG data', {
   file_prefix <- 'new-autompg'
   set.seed(seed)
   cat(sprintf('-- Data: %s -----------------------', file_prefix), '\n')
+  
   cat(sprintf('-- Model: nnet -----------------------'), '\n')
-  # TODO: avoid declaring duplicate parameters
   uci_test(file_prefix, nnet, size=6, linout=T, trace=F)
+  
   cat(sprintf('-- Model: AdaBoost.R2 with nnet ------'), '\n')
   uci_test(file_prefix, adaboostR2, size=6, linout=T, trace=F)
+  
+  cat(sprintf('-- Model: TrAdaBoost.R2 with nnet ------'), '\n')
+  uci_test(file_prefix, trAdaboostR2, size=6, linout=T, trace=F)
 })
 
 # TODO: fix error "factor c has new levels chevrolet, honda"
